5d881fc534ecd933a35d9daf5630247b03b3f7aa
Eltaweeel <abdotaweel1@gmail.com>
2021-01-25 15:28:15 +0100

    adding comment to third project

--- Codezilla_Projects/project_3.py
+++ Codezilla_Projects/project_3.py
@@ -1,3 +1,5 @@
+"""In this project we are asking user to enter any text and we will check how many vowels in this text and how many times this vowel has repitated"""
+
 string = input ("Please enter any text: ")
 text = string.lower()
 
