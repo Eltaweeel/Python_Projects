b70f013377d16b3f98413c2a8382803959fa6161
Eltaweeel <abdotaweel1@gmail.com>
2021-01-25 15:24:53 +0100

    adding comment to first project

--- Codezilla_Projects/project_1.py
+++ Codezilla_Projects/project_1.py
@@ -1,3 +1,5 @@
+"""in this project we are asking user to enter any word, then we are going to reverse the word. So first letter will be last and son on"""
+
 first_word = input("Please enter the word: \n")
 final_word = first_word[::-1]
 print("the reversed word is: {}".format(final_word))
