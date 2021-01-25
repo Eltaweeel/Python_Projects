2b98271cc281335471df9d415534f291c3403341
Eltaweeel <abdotaweel1@gmail.com>
2021-01-25 15:25:54 +0100

    adding comment to second project

--- Codezilla_Projects/project_2.py
+++ Codezilla_Projects/project_2.py
@@ -1,3 +1,5 @@
+"""In this project we are asking the user to enter any word so we can check if is palindrome or not"""
+
 word = input("Please enter a word to check palindromic or not: \n")
 word = word.lower()
 if word == word[::-1]:
